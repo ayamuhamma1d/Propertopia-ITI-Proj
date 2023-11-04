e2ac567304d26ced5f48e7656ae0dfec7b565083
Esraa-Mohamed-Mostafa <124133454+Esraa-Mohamed-Mostafa@users.noreply.github.com>
2023-11-05 00:13:21 +0200

    service-ui-esraa

--- src/components/home/Services.jsx
+++ src/components/home/Services.jsx
@@ -4,7 +4,7 @@ const Services = () => {
     return (
         <>
         <section>
-            <div class="w-full sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-5 mx-auto mb-10 text-center">
+            <div class="w-full sm:max-w-xl md:max-w-full lg:max-w-screen-lg md:px-5 mx-auto mb-10 text-center">
                 <h2 class="font-bold text-3xl font-[Poppins]">Services</h2>
                 <div class="mt-10 flex flex-col space-x-0 space-y-5 md:flex-row md:space-x-5 md:space-y-0 space-x-5 ">
 
@@ -20,7 +20,6 @@ const Services = () => {
                     </a>
                     </div>
                     </div>
-
                     <div class=" md:w-1/3 flex flex-col items-center p-6 space-y-4 rounded-lg border border-slate-200">
                         <img src={photo} alt="1" class="w-full" />
                         <h5 class="font-bold text-lg text-slate-900"> Buy a home</h5>
