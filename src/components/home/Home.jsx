43f0302e8e4d38a3758dd74b02ac4c9bfee05778
Aya Muhammad Mansour <aya676670@gmail.com>
2023-11-05 16:23:46 +0200

    check project work --aya

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import Contact from "./../contact";
 import Maintenance from "./Maintenance";
 import Services from "./Services";
-
-
+import Hero from "./Hero";
 const Home = () => {
   return (
     <>
+      <Hero />
       <Services />
       <Maintenance />
       <Contact />
     </>
   );
 };
-
 export default Home;
