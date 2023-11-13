cdbf2dc90b463028e7a747a669487712418fe6d2
Aya Muhammad Mansour <aya676670@gmail.com>
2023-11-13 13:35:18 +0200

    edit service

--- src/components/wishlist/Wishlist.jsx
+++ src/components/wishlist/Wishlist.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+
+import React, { useEffect } from 'react';
+import { db } from './../auth/firebase/Firebase';
+import { doc, collection, getDoc } from 'firebase/firestore';
 
 const Wishlist = () => {
-  return (
-    <div>Wishlist</div>
-  )
-}
+  useEffect(() => {
+    const fetchWishlistData = async () => {
+      try {
+        const cityRef = db.collection("wishlist")
 
-export default Wishlist
+        const doc = await cityRef.get();
+        if (!doc.exists) {
+          console.log('No such document!');
+        } else {
+          console.log('Document data:', doc.data());
+        }
+      } catch (error) {
+        console.log('Error getting wishlist data:', error);
+      }
+    };
+
+    fetchWishlistData();
+  }, []);
+
+  return <div>Wishlist</div>;
+};
+
+export default Wishlist;
