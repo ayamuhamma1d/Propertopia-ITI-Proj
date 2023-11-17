8d4471a811a314e769d7059a9143a436ef1b764b
Pancee Ahmed <Panceeahmed22@gmail.com>
2023-11-17 20:45:10 +0200

    update loader funct.

--- src/Spinner/withLoadingSpinner.jsx
+++ src/Spinner/withLoadingSpinner.jsx
@@ -10,58 +10,72 @@ const withLoadingSpinner = (WrappedComponent, firebase) => {
       const fetchData = async () => {
         try {
           setLoading(true);
+
+          // Fetch data from Firebase
           const firebaseData = await data.fetchData();
 
+          // Collect all image, video, and iframe URLs
+          const allUrls = [];
+
+          // Handle local assets
           const assetsContext = require.context(
             "../../src/assets/",
             false,
             /\.(png|jpg|jpeg|gif|mp4|webm)$/
           );
-
-          const allFiles = assetsContext.keys().map((file) => {
-            return {
-              type:
-                file.includes(".mp4") || file.includes(".webm")
-                  ? "video"
-                  : "image",
-              path: assetsContext(file).default,
-            };
+          const localFiles = assetsContext.keys().map((file) => {
+            const path = assetsContext(file).default;
+            if (path.endsWith(".png") || path.endsWith(".jpg") || path.endsWith(".jpeg") || path.endsWith(".gif") || path.endsWith(".mp4") || path.endsWith(".webm")) {
+              allUrls.push(path);
+            }
+            return path;
           });
 
-          const filePromises = allFiles.map((file) => {
+          // Handle image and video URLs from Firebase
+          const firebaseFiles = Object.values(firebaseData).map((item) => {
+            if (item.type === "image" || item.type === "video") {
+              allUrls.push(item.url);
+            }
+            return item.url;
+          });
+
+          // Handle iframe URLs from Firebase
+          const firebaseIframes = Object.values(firebaseData).map((item) => {
+            if (item.type === "iframe") {
+              allUrls.push(item.src);
+            }
+            return item.src;
+          });
+
+          // Load all URLs
+          const filePromises = allUrls.map((url) => {
             return new Promise((resolve, reject) => {
-              if (file.type === "image") {
+              if (url.endsWith(".png") || url.endsWith(".jpg") || url.endsWith(".jpeg") || url.endsWith(".gif")) {
                 const img = new Image();
-                img.onload = () => {
-                  if (img.complete) {
-                    resolve();
-                  } else {
-                    img.onload = resolve;
-                  }
-                };
+                img.onload = resolve;
                 img.onerror = reject;
-                img.src = file.path;
-              } else if (file.type === "video") {
+                img.src = url;
+              } else if (url.endsWith(".mp4") || url.endsWith(".webm")) {
                 const video = document.createElement("video");
-                video.onloadeddata = () => {
-                  if (video.readyState >= 3) {
-                    resolve();
-                  } else {
-                    video.onloadeddata = resolve;
-                  }
-                };
+                video.onloadeddata = resolve;
                 video.onerror = reject;
-                video.src = file.path;
+                video.src = url;
                 document.body.appendChild(video);
+              } else if (url.endsWith(".html")) { // Assuming iframe URLs end with .html
+                const iframe = document.createElement("iframe");
+                iframe.onload = resolve;
+                iframe.onerror = reject;
+                iframe.src = url;
+                document.body.appendChild(iframe);
+              } else {
+                resolve(); // Skip non-supported URLs
               }
             });
           });
 
-          const delayPromise = new Promise((resolve) => {
-            setTimeout(resolve, 100);
-          });
+          // Wait for all promises to resolve
+          await Promise.all([...filePromises]);
 
-          await Promise.all([firebaseData, ...filePromises, delayPromise]);
         } catch (error) {
           console.error("Error fetching data or loading files:", error);
         } finally {
@@ -71,6 +85,35 @@ const withLoadingSpinner = (WrappedComponent, firebase) => {
 
       fetchData();
     }, [firebase]);
+
+    const handleIntersection = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setLoading(false);
+        }
+      });
+    };
+
+    useEffect(() => {
+      const observer = new IntersectionObserver(handleIntersection, {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.1, // Adjust this threshold as needed
+      });
+
+      // Select all images in the document
+      const images = document.querySelectorAll("img");
+
+      // Observe each image
+      images.forEach((img) => {
+        observer.observe(img);
+      });
+
+      return () => {
+        // Cleanup: disconnect the observer when the component unmounts
+        observer.disconnect();
+      };
+    }, []);
 
     return (
       <div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import App from './App';
-import axios from 'axios';
- import "./index.css";
-import ReactDOM from "react-dom/client";
- import "flowbite";
-import withLoadingSpinner from './Spinner/withLoadingSpinner';
+import React from "react";
+import App from "./App";
+import axios from "axios";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "flowbite";
+import withLoadingSpinner from "./Spinner/withLoadingSpinner";
 
 const AppWithLoadingSpinner = withLoadingSpinner(App, axios);
 
-const rootElement = document.getElementById('root');
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.render(
   <React.StrictMode>
     <AppWithLoadingSpinner />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
