e365afb0b99701fba77619197a3ab9efd3418da2
Mahmoud Omar <momar351@yahoo.com>
2023-11-20 01:02:09 +0200

    chamges in services --mahmoud

--- src/components/home/Services.jsx
+++ src/components/home/Services.jsx
@@ -1,61 +1,99 @@
-import React from 'react';
-import './services.css';
-import dphoto from '../../../src/assets/img/Maintenance1/1 (2).jpg';
-import photosellrent from '../../../src/assets/img/Maintenance1/1 (12).jpg';
-import photo3d from '../../../src/assets/img/pngegg.png';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./services.css";
+import dphoto from "../../../src/assets/img/Maintenance1/1 (2).jpg";
+import photosellrent from "../../../src/assets/img/Maintenance1/1 (12).jpg";
+import photo3d from "../../../src/assets/img/pngegg.png";
+import { Link } from "react-router-dom";
+
+const paragraph = [
+  "Uncover Your Oasis: Immerse Yourself in a Visual Journey to Your Ideal Home. Unlock Unparalleled Properties, Tailored for Renting", "Revitalize Your Space: Elevate Living with Our Exclusive Maintenance Magic. From Top-Notch Upkeep to Seamless Repairs .", " Revolutionize Your Showcase: Step into the Future with Our State-of-the-Art 3D Services. Immerse Your Audience in Captivating Virtual Tours "
+]
+
 const Services = () => {
   return (
     <>
-      <section>
-        <div className="w-full sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-5 mx-auto mb-10 text-center">
-        <h5 className="font-bold text-3xl font-serif capitalize mb-10 text-center">Services</h5>
-          <div className="flex flex-col gap-5 md:flex-row md:space-x-5 md:space-y-0 space-x-5 justify-center items-center">
-            <div className="md:w-1/3 flex flex-col items-center  space-y-4 rounded-lg border border-slate-200">
-              <img src={photosellrent} alt="1" className="" />
-              <h3 className="font-bold text-xl text-slate-900 font-serif capitalize"> Own or Rent </h3>
-              <p className="my-8 px-4  leading-5 font-serif text-sm text-slate-700">
-              Uncover Your Oasis: Immerse Yourself in a Visual Journey to Your Ideal Home. Unlock Unparalleled Properties, Tailored for Renting Flexibility or Forever Ownership.</p>
-              <div>
-              <Link to="/service"            
-                  className="inline-flex mb-5 hover:bg-beige hover:text-white items-center px-3 py-2 text-sm font-medium text-center text-black service__button__background__color"
-                >
-                  Read more
-                </Link>
+      <div className="w-full sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-5 mx-auto mb-10 text-center">
+      <h5 className="font-bold text-3xl font-serif capitalize mb-10 text-center">Services</h5>        <div className="flex  gap-10  justify-center items-center">
+          <div className="flex  gap-10  justify-center items-center services_cards ">
+            <div className="w-full max-w-sm font-[Poppins] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="relative ">
+                <img src={photosellrent} alt="" />
               </div>
-            </div>
-            <div className="md:w-1/3 flex flex-col items-center  space-y-4 rounded-lg border border-slate-200">
-              <img src={dphoto} alt="1" className='' />
-              <h3 className="font-bold text-xl text-slate-900 font-serif capitalize">Maintenance </h3>
-              <p className="my-8  px-2 leading-5 text-sm font-serif text-slate-700">
-              Revitalize Your Space: Elevate Living with Our Exclusive Maintenance Magic. From Top-Notch Upkeep to Seamless Repairs .
+              <div className="px-5 pb-5">
+                <div className="flex items-center justify-between mb-3 pt-3"></div>
+                <div>
+                <h3 className="font-bold text-xl text-slate-900 font-serif capitalize">
+                    Buy or rent
+                  </h3>
+
+                  <p className="my-8 px-4  leading-5 font-serif text-sm text-slate-700 services_paragraph">
+             {paragraph[0].split(' ').slice(0, 19).join(' ')}
               </p>
-              <div>
-                <Link to="/service"    
-                  className="inline-flex mb-5 hover:bg-beige hover:text-white items-center px-3 py-2 text-sm font-medium text-center text-black service__button__background__color"
-                >
-                  Read more
-                </Link>
+                  <div>
+                    <Link
+                      to="/service"
+                      className="inline-flex mb-5 hover:bg-beige hover:text-white items-center px-3 py-2 text-sm font-medium text-center text-black service__button__background__color"
+                    >
+                      Read more
+                    </Link>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="md:w-1/3 flex flex-col items-center  space-y-4 rounded-lg border border-slate-200">
-              <img src={photo3d} alt="1" className="w-full" />
-              <h3 className="font-bold text-xl text-slate-900 font-serif capitalize"> 3D Module</h3>
-              <p className="my-8  px-2 leading-5 text-sm font-serif  text-slate-700">
-              Revolutionize Your Showcase: Step into the Future with Our State-of-the-Art 3D Services. Immerse Your Audience in Captivating Virtual Tours .  </p>
-              <div>
-                <Link to="/service"
-                 
-                  className="inline-flex mb-5 hover:bg-beige hover:text-white items-center px-3 py-2 text-sm font-medium text-center text-black service__button__background__color"
-                >
-                  Read more
-                </Link>
+            <div className="w-full max-w-sm font-[Poppins] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="relative ">
+                <img src={dphoto} alt="" />
+              </div>
+              <div className="px-5 pb-5">
+                <div className="flex items-center justify-between mb-3 pt-3"></div>
+                <div>
+                  <h3 className="font-bold text-xl text-slate-900 font-serif capitalize">
+                    MAINTENANCE
+                  </h3>
+                  <p className="my-8  px-2 leading-5 text-sm font-serif text-slate-700 services_paragraph">
+                  {paragraph[1].split(' ').slice(0, 19).join(' ')}
+              </p>
+
+                  <div>
+                    <Link
+                      to="/service"
+                      className="inline-flex mb-5 hover:bg-beige hover:text-white items-center px-3 py-2 text-sm font-medium text-center text-black service__button__background__color"
+                    >
+                      Read more
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full max-w-sm font-[Poppins] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="relative ">
+                <img src={photo3d} alt="" />
+              </div>
+              <div className="px-5 pb-5">
+                <div className="flex items-center justify-between mb-3 pt-3"></div>
+                <div>
+                  <h3 className="font-bold text-xl text-slate-900 font-serif capitalize">3D</h3>
+                  <p className="my-8  px-2 leading-5 text-sm font-serif  text-slate-700 services_paragraph">
+                  {paragraph[2].split(' ').slice(0, 19).join(' ')}
+
+               </p>
+                  <div>
+                    <Link
+                      to="/service"
+                      className="inline-flex mb-5 hover:bg-beige hover:text-white items-center px-3 py-2 text-sm font-medium text-center text-black service__button__background__color"
+                    >
+                      Read more
+                    </Link>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
     </>
   );
 };
+
 export default Services;
+
