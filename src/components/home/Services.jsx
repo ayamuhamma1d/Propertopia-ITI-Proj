b04dd3d9c47db955a378616d5b5e1ee9e7c0aac1
Mahmoud Omar <momar351@yahoo.com>
2023-11-20 06:59:43 +0200

    some changes in services --mahmoud

--- src/components/home/Services.jsx
+++ src/components/home/Services.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./services.css";
 import dphoto from "../../../src/assets/img/Maintenance1/1 (2).jpg";
 import photosellrent from "../../../src/assets/img/Maintenance1/1 (12).jpg";
-import photo3d from "../../../src/assets/img/3d-rendering-isometric-house.jpg";
+import photo3d from "../../../src/assets/img/REODECOR7.jpg";
 import { Link } from "react-router-dom";
 const paragraph = [
   "Uncover Your Oasis: Immerse Yourself in a Visual Journey to Your Ideal Home. Unlock Unparalleled Properties, Tailored for ", "Revitalize Your Space: Elevate Living with Our Exclusive Maintenance Magic. From Top-Notch Upkeep to Seamless Repairs .", " Revolutionize Your Showcase: Step into the Future with Our State-of-the-Art 3D Services. Immerse Your Audience in Captivating Virtual Tours "
@@ -15,7 +15,7 @@ const Services = () => {
           <div className="flex  gap-10  justify-center items-center services_cards ">
             <div className="w-full max-w-sm font-serif bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <div className="relative ">
-                <img src={photosellrent}  />
+                <img src={photosellrent} id="serv_images" />
               </div>
               <div className="px-5 pb-5">
                 <div className="flex items-center justify-between mb-3 pt-3"></div>
@@ -39,7 +39,7 @@ const Services = () => {
             </div>
             <div className="w-full max-w-sm font-serif bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <div className="relative ">
-                <img src={dphoto} alt="" />
+                <img src={dphoto}  id="serv_images" />
               </div>
               <div className="px-5 pb-5">
                 <div className="flex items-center justify-between mb-3 pt-3"></div>
@@ -63,7 +63,7 @@ const Services = () => {
             </div>
             <div className="w-full max-w-sm font-serif bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <div className="relative ">
-                <img src={photo3d} alt="" />
+                <img src={photo3d} id="serv_images"/>
               </div>
               <div className="px-5 pb-5">
                 <div className="flex items-center justify-between mb-3 pt-3"></div>
